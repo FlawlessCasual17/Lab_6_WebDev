fd74481b673b2a20cd76a8fbb0d2bccfd89db666
Zliced13 <07e5297d5b@c0x0.com>
2023-12-06 21:52:11 -0700

    Here is a commit message for the git diff:
    :wrench: Refactor robot rental site code
    
    This commit refactors the code for the robot rental website. The main changes are:
    
    - Add JSHint configuration file and fix linting issues
    - Restructure JavaScript code with clearer naming and formatting
    - Update CSS reset formatting for consistency
    
    The refactor improves readability, maintainability and formatting consistency of the codebase. No functional changes were made, the site functionality remains the same.
    
    Signed-off-by: Zliced13 <07e5297d5b@c0x0.com>

--- Lab_6/scripts/scripts.js
+++ Lab_6/scripts/scripts.js
@@ -1,17 +1,13 @@
 /****************** YOUR NAME: Kris Schneider
-
  The instructions describe the missing logic that is needed;
  you will translate these into JavaScript in the places indicated.
-
  You are encouraged to use the provided naming convention for ease of review.
  */
-
 /****************** create variables ******************/
 /* create variables to hold the values for modelName and duration */
-
 // INSERT YOUR CODE HERE
-let modelName
-let duration
+let modelName = '', duration = 0
+
 
 /****************** helper function ******************/
 /* Create a function called recalculate() which will
@@ -24,43 +20,92 @@ let duration
  */
 
 // INSERT YOUR CODE HERE
-function Recalculate () {
+function Recalculate() {
+    // A variable that holds an HTMLElement value.
     let costLabel = document
         .getElementById("calculated-cost")
+    let newTotalCost // Represents the NEW total cost.
 
-    let newTotalCost
+    switch (modelName) { // @formatter:off
+        /*
+            Set the value of `newTotalCost` to 100
+            times the duration when `modelName`
+            equals 'XYZ'
+         */
+        case 'XYZ':
+            newTotalCost = duration * 100; break
+        /*
+            Set the value of `newTotalCost` to 213
+            times the duration when `modelName`
+            equals 'CRPG'
+         */
+        case 'CRPG':
+            newTotalCost = duration * 213; break
+    } // @formatter:on
 
-
-    if (modelName === 'XYZ') newTotalCost = duration * 100
-    else if (modelName === 'CRPG') newTotalCost = duration * 213
-
+    // Sets `HTMLElement.innerHTML` to the NEW total cost.
     costLabel.innerHTML = newTotalCost
 }
 
+
 /****************** model button logic ******************/
-
+// @formatter:off
 /*
- - first, create a variable to represent the "Switch Model" pseudo-button (hint: can use getElementById)
- - second, create a function called changeModel() which checks the value of the model name variable. This function will:
- - create a variable to represent the model-text span element
- - if modelName is currently "XYZ", change the value of modelName to "CPRG", and change the innerHTML of the model-text span element to "Model CPRG"
- - if modelName is currently "CPRG", change the value of modelName to "XYZ", and change the innerHTML of the model-text span element to "Model XYZ"
- - then, recalculate() the total cost.
- - finally, uncomment the following line of JavaScript to have this function run automatically whenever the pseudo-button is clicked: */
-// modelButton.addEventListener("click", changeModel);
-
+ * first, create a variable to represent the "Switch Model"
+ pseudo-button (hint: can use getElementById)
+ * second, create a function called changeModel() which checks the
+ value of the model name variable.
+ * This function will:
+    - create a variable to represent the model-text span element
+    - if modelName is currently "XYZ", change the value of modelName to "CPRG",
+        and change the innerHTML of the model-text span element to "Model CPRG"
+    - if modelName is currently "CPRG", change the value of modelName to "XYZ",
+        and change the innerHTML of the model-text span element to "Model XYZ"
+    - then, recalculate() the total cost.
+ * finally, uncomment the following line of JavaScript to have
+ this function run automatically whenever the pseudo-button is clicked:
+ // modelButton.addEventListener("click", changeModel);
+ */
+// @formatter:on
 // INSERT YOUR CODE HERE
+let modelButton = document.getElementById("switchButton")
+
+modelName = 'XYZ'
+
+function ChangeModel() {
+    let modelText = document.getElementById("model-text")
+
+    switch (modelName) { // @formatter:off
+        case 'XYZ':
+            modelName = 'CPRG'
+            modelText.innerHTML = 'Model CPRG'
+            break
+        case 'CPRG':
+            modelName = 'XYZ';
+            modelText.innerHTML = 'Model XYZ'
+            break
+    } // @formatter:on
+
+    Recalculate() // Recalculating the total cost.
+}
+
+modelButton.addEventListener("click", ChangeModel)
 
 
 /****************** duration button logic ******************/
-/*  - first, create a variable to represent the "Change Duration" pseudo-button.
+// @formatter:off
+
+/*
+ - first, create a variable to represent the "Change Duration" pseudo-button.
  - then, create a function called changeDuration() that will
- - create a variable to represent the duration-text span element
- - prompt() the user for a new duration
- - save the result of the prompt() to the duration variable
- - change the innerHTML of the duration-text span element to this new value
- - recalculate() the total cost/
+    - create a variable to represent the duration-text span element
+    - prompt() the user for a new duration
+    - save the result of the prompt() to the duration variable
+    - change the innerHTML of the duration-text span element to this new value
+    - recalculate() the total cost/
  - finally, attach this function to the "Change Duration" pseudo-button, so it runs whenever the button is clicked.
  */
+
+// @formatter:on
 
 // INSERT YOUR CODE HERE
